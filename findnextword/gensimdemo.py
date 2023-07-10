caeb90f285b103f5e60972a20865c801339368de
Zach <zaclewis@gmail.com>
2023-07-10 11:25:52 -0500

    init5

--- findnextword/gensimdemo.py
+++ findnextword/gensimdemo.py
@@ -1,4 +1,15 @@
+#
+# CHATGPT Generated Python Code
+#
+# Prompt: Explain word2vec in bullet form and a simple example
+
+# In this example, we have a corpus of sentences represented as a list of lists. We use the Word2Vec class from the Gensim library to train the Word2Vec model on the given sentences.
+# After training, we can access word embeddings using the wv attribute of the model. We retrieve the embeddings for specific words like 'chocolate' and 'ice' and store them in variables.
+# We can also find similar words to a given word using the most_similar method. In this case, we find similar words to 'chocolate' and print the results.
+# Additionally, we demonstrate word arithmetic by finding the word that is similar to 'chocolate' and 'ice' but dissimilar to 'eating'. The `most
+
 from gensim.models import Word2Vec
+
 
 # Corpus of sentences
 sentences = [['I', 'love', 'chocolate'],
